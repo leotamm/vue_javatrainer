c2704fa27eb010418d7ac3605fa5dfe562f350a8
Leo Tamm <leo.tamm@gmail.com>
2020-11-30 16:15:20 +0200

    radio töötab

--- src/views/About.vue
+++ src/views/About.vue
@@ -6,25 +6,27 @@
     <p>
       <button v-on:click="getName()">Genereeri test</button>
     </p>
-    <div v-for="a in questionSet">
-      Question q_id: {{a.q_id}}
-      Question question: {{a.question}}
+
+    <table width="500px" align="centre" border="1" v-for="(a, index) in questionSet">
+      <tr><td>Küsimus {{index+1}}: {{ a.question }}</td></tr>
+      <tr>
       <div v-for="b in a.answers">
-        Answer answerId: {{b.answerId}}
-        Answer answer: {{b.answer}}
+
+        <td>{{ b.answer }}</td>
+          <td><input id="radio1" type="radio" v-model="a.selectedAnswer" v-bind:value="b.answerId" v-bind:name="index"></td>
+
       </div>
-    </div>
+      </tr>
+    </table>
+  {{questionSet}}
   </div>
-
 </template>
-
 
 <script>
 
 let getName = function () {
   this.$http.get("http://localhost:8090/trainer/testpackage")
       .then(result => this.questionSet = result.data)
-
 }
 
 export default {
