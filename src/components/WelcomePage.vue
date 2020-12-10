313c22cc4a7dd8135c3172b8b76d1ef9ff3bbb3a
Leo Tamm <leo.tamm@gmail.com>
2020-12-10 09:17:33 +0200

    uus avaleht

---
+++ src/components/WelcomePage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    <h2>{{ message }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WelcomePage',
+  props: {
+    message: String,
+    message2: String
+  }
+}
+</script>
+
+<style scoped>
+h1{
+  color: #197815
+}
+h2{
+  color:darkgrey;
+}
+</style>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home" style="background-color:#efeaf0">
+    <br>
+    <img alt="School Logo" width="100" src="../assets/school-icon_2.png">
+    <WelcomePage message="Tere tulemast javaTrainerit kasutama!"/>
+    <br>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import WelcomePage from '@/components/WelcomePage'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    WelcomePage
   }
 }
 </script>
